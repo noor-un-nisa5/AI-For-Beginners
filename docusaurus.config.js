9150a1b9b6abb9be3e0714a33377734762f88e4e
Noor Un Nisa <163582696+noor-un-nisa5@users.noreply.github.com>
2025-12-19 03:52:31 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://noor-un-nisa5.github.io',
+  url: 'https://ai-for-beginners.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AI-For-Beginners/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
